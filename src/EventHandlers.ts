9277da7e0876bc33745ec999afc6025c8762ae8d
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2024-10-21 12:04:34 +0200

    update

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -20,27 +20,31 @@ import {
 //   context.Core_OddsChanged.set(entity);
 // });
 
-FreeBet.NewBet.handler(async ({ event, context }) => {
-  const entity: FreeBet_NewBet = {
-    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    freeBetId: event.params.freeBetId,
-    core: event.params.core,
-    bettor: event.params.bettor,
-    azuroBetId: event.params.azuroBetId,
-    amount: event.params.amount,
-    minOdds: event.params.minOdds,
-    expiresAt: event.params.expiresAt,
-  };
+// FreeBet.NewBet.handler(async ({ event, context }) => {
+//   const entity: any = {
+//     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+//     freeBetId: event.params.freeBetId,
+//     core: event.params.core,
+//     bettor: event.params.bettor,
+//     azuroBetId: event.params.azuroBetId,
+//     amount: event.params.amount,
+//     minOdds: event.params.minOdds,
+//     expiresAt: event.params.expiresAt,
+//     blockNumber: event.block.number || 0n,
+//     blockTimestasmp: event.block.timestamp || 0n,
+//   };
 
-  context.FreeBet_NewBet.set(entity);
-});
+//   context.FreeBet_NewBet.set(entity);
+// });
 
 PrematchCore.OddsChanged.handler(async ({ event, context }) => {
   const entity: PrematchCore_OddsChanged = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     conditionId: event.params.conditionId,
     newOdds: event.params.newOdds,
-  };
+    blockNumber: event.block.number,
+    blockTimestamp: event.block.timestamp,
+  }; 
 
   context.PrematchCore_OddsChanged.set(entity);
 });
